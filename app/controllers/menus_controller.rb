f2df93bc9a2a43d9f99db989129aa1d594ccf29f
tech-kannnari <madpowertk@gmail.com>
2021-04-05 15:07:47 +0900

    ルーティング、コントローラーの修正

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,7 +1,23 @@
 class MenusController < ApplicationController
 
+  before_action :authenticate_user!, only: [:new, :create]
+
 
   def index
+    # @dinner = Dinner.all.order("RAND()").limit(6)
+  end
+  
+  def new
+    @menu = Menu.new
+  end
+  
+  def create
+    @menu = Menu.new(menu_params)
+    if @menu.save
+      redirect_to root_path
+    else
+      render:new
+    end
   end
 
   def new_guest
@@ -13,6 +29,12 @@ class MenusController < ApplicationController
     else
       redirect_to root_path, notice: 'ダメでした'
     end
+  end
+
+  private
+
+  def menu_params
+    params.require(:menu).permit(:main_name, :main_recipe, :main_genre, :salad_name, :salad_recipe, :salad_genre, :soup_name, :soup_recipe, :soup_genre)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "menus#index"
-  resources :menus, only:[:index]
+  resources :menus, only:[:index, :new, :create]
   post '/menus/guest_sign_in', to: 'menus#new_guest'
 end
