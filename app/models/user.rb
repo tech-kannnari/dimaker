6ff4d093b406da9df022825d6819507be9cc8e44
tech-kannnari <madpowertk@gmail.com>
2021-04-05 15:42:07 +0900

    アソシエーションの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
+  has_many :menus
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
          with_options presence: true do
           validates :nickname
           validates :encrypted_password
