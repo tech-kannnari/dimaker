1a6e08566dcc449f1d46c798c3f1cf5444060019
tech-kannnari <madpowertk@gmail.com>
2021-04-05 19:16:06 +0900

    バリデーション、アソシエーションの記述

---
+++ app/models/dinner.rb
@@ -0,0 +1,10 @@
+class Dinner < ApplicationRecord
+  belongs_to :user
+  has_one_attached :image
+
+  with_options presence: true do
+    validates :impression
+    validates :image
+    validates :cookday
+  end    
+end
