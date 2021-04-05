68ee85cd6b92eb2b40d5970a7078f63a0132a97d
tech-kannnari <madpowertk@gmail.com>
2021-04-05 19:15:41 +0900

    dinnerコントローラー、ルーティングの記述

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   root to: "menus#index"
   resources :menus, only:[:index, :new, :create]
   post '/menus/guest_sign_in', to: 'menus#new_guest'
+  resources :dinners, only:[:new, :create]
 end
