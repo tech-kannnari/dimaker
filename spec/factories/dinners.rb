bbf36c442eefa38aab031b063a34cf178d282098
tech-kannnari <madpowertk@gmail.com>
2021-04-05 19:40:42 +0900

    dinnerモデルのテストコード

--- spec/factories/dinners.rb
+++ spec/factories/dinners.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :dinner do
-    
+    impression { '調理完了' }
+    cookday    { Faker::Date }
+    association :user
+
+    after(:build) do |dinner|
+      dinner.image.attach(io: File.open('public/images/omuraisu.png'), filename: 'omuraisu.png')
+    end
   end
 end
--- spec/models/dinner_spec.rb
+++ spec/models/dinner_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Dinner, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @dinner = FactoryBot.build(:dinner)
+  end
+
+  describe '新規recordの登録' do
+    context '新規recordの登録ができるとき' do
+      it 'image,impression,cookdayが存在すれば登録できる' do
+        expect(@dinner).to be_valid
+      end
+    end
+    context '新規recordの登録ができないとき' do
+      it 'imageが存在しない時登録できない' do
+        @dinner.image = nil
+        @dinner.valid?
+        expect(@dinner.errors.full_messages).to include("Image can't be blank")
+      end
+      it 'impressionが存在しない時登録できない' do
+        @dinner.impression = nil
+        @dinner.valid?
+        expect(@dinner.errors.full_messages).to include("Impression can't be blank")
+      end
+      it 'cookdayが存在しない時登録できない' do
+          @dinner.cookday = nil
+          @dinner.valid?
+          expect(@dinner.errors.full_messages).to include("Cookday can't be blank")
+      end
+    end
+  end
 end
