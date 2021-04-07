41cc04a3a22b1c8de62e9061f8116c7fd1662d26
tech-kannnari <madpowertk@gmail.com>
2021-04-07 14:55:46 +0900

    showアクション、ルーティングの追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   root to: "menus#index"
   resources :menus, only:[:index, :new, :create]
   post '/menus/guest_sign_in', to: 'menus#new_guest'
-  resources :dinners, only:[:index, :new, :create]
+  resources :dinners, only:[:index, :new, :create, :show]
 end
